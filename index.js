fa3ac9ffde370de3e3be7d8210e9424e6ef92ae4
rmkofficial <kmert0@outlook.com>
2023-02-03 13:10:15 +0300

    added style

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumsButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
-  function handleClick() {
-    alert("I got clicked!");
-  }
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function () {
+        this.style.color = "black";
+    });
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//     audio.play();
